c6456e45d6e188c6e253529b57554a90a5fd2574
ashkuc <ashkucc@gmail.com>
2023-12-18 23:26:52 +0100

    added todo about file buffer

--- src/types/storage.ts
+++ src/types/storage.ts
@@ -2,5 +2,6 @@ import { TokenInfo } from './token';
 
 export interface UploadFile {
   token: TokenInfo;
+  // todo - do not use path and dumping file to disk, use buffer instead
   filePath: string;
 }
