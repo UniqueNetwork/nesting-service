59195786b1a7e710e251ce7239a3ba56a04c6006
ashkuc <ashkucc@gmail.com>
2023-12-20 19:28:05 +0100

    minor refactoring of subscriber.service.ts

--- src/modules/subscriber/subscriber.service.ts
+++ src/modules/subscriber/subscriber.service.ts
@@ -1,11 +1,6 @@
 import { Inject, Injectable, Logger } from '@nestjs/common';
 import { SdkService } from '../sdk';
-import {
-  ChainType,
-  RmqPatterns,
-  RmqServiceNames,
-  TokenInfo,
-} from '../../types';
+import { ChainType, RmqPatterns, RmqServiceNames, TokenInfo } from '../../types';
 import { CollectionData } from '@unique-nft/sdk';
 import { Address } from '@unique-nft/utils';
 import { ClientProxy } from '@nestjs/microservices';
@@ -23,30 +18,30 @@ export class SubscriberService {
     sdk.subscribe(this.onEvent.bind(this));
   }
 
-  private extractTokenFromAddress(
-    chain: ChainType,
-    address: string,
-  ): TokenInfo | null {
-    return address && Address.is.nestingAddress(address)
-      ? {
-          chain,
-          ...Address.nesting.addressToIds(address),
-        }
-      : null;
+  private extractTokenFromAddress(chain: ChainType, address?: string): TokenInfo | null {
+    if (!address || !Address.is.nestingAddress(address)) return null;
+
+    const { collectionId, tokenId } = Address.nesting.addressToIds(address);
+
+    return { chain, collectionId, tokenId };
   }
 
   private async onEvent(chain: ChainType, eventData: CollectionData) {
+    const {
+      event: { method, section, block },
+    } = eventData;
+
+    this.logger.log(`Received event ${section}.${method} on ${chain}. Block: ${block?.id}`);
+
     const { address, addressTo } = eventData.parsed;
 
-    const parentToken: TokenInfo | null =
-      this.extractTokenFromAddress(chain, address) ||
-      this.extractTokenFromAddress(chain, addressTo);
+    const parentToken = this.extractTokenFromAddress(chain, address) || this.extractTokenFromAddress(chain, addressTo);
 
     if (!parentToken) {
+      this.logger.log(`No token found, ignoring event.`);
+
       return;
     }
-
-    this.logger.log(`parse event`, parentToken);
 
     const bundle = await this.sdk.getBundle(parentToken);
 
@@ -56,15 +51,17 @@ export class SubscriberService {
       tokenId: bundle.tokenId,
     };
 
-    const sendResult = this.rmqClient.emit<any, TokenInfo>(
-      RmqPatterns.BUILD_TOKEN,
-      rootToken,
-    );
+    this.enqueueToken(rootToken);
+  }
+
+  private enqueueToken(token: TokenInfo) {
+    const sendResult = this.rmqClient.emit<any, TokenInfo>(RmqPatterns.BUILD_TOKEN, token);
 
     sendResult
       .pipe(
         catchError((err) => {
-          this.logger.error('fail add token to queue', eventData, err);
+          this.logger.error('failed add token to queue', token, err);
+
           return err;
         }),
       )
